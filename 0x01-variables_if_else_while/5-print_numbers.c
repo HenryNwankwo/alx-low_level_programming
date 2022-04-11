b5dfec23b7171fdc5cb08df2af0e8614d507e864
momeniHIN <nameisifunanya@gmail.com>
2022-04-10 19:41:08 -0700

    Created single digit number file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	return (0);
+}
