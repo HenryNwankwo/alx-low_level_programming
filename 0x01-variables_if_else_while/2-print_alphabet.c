90028bdc9aab2ab7cbdfb4e46508aa8006484a90
momeniHIN <nameisifunanya@gmail.com>
2022-04-10 18:20:22 -0700

    Created print alphabet file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = "a"; ch <= "z"; ++ch)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+	return (0);
+}
