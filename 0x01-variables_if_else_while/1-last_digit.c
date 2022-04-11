f00cd7c05064787d6b372301a938752286b59e86
momeniHIN <nameisifunanya@gmail.com>
2022-04-10 17:50:49 -0700

    Created last digit if else file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d", n);
+	if (n % 10 > 5)
+	{
+		printf("is %d and is greater than 5\n", n % 10);
+	} else if (n % 10 == 0)
+	{
+		printf("is %d and is 0\n", n % 10);
+	} else
+	{
+		printf("is %d less than 6 and not zero\n", n % 10);
+	}
+	return (0);
+}
