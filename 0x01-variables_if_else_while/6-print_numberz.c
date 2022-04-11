f80464e5c83f6eb97f3b13cfca75f35e20ca48c0
momeniHIN <nameisifunanya@gmail.com>
2022-04-10 20:10:49 -0700

    Created the number file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; ++num)
+	{
+		putchar(num + '0');
+	}
+	putchar('\n');
+	return (0);
+}
